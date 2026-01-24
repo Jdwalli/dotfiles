c48a1c27ef3de01d2a66a9a34a6ec2d7b63d5d95
Joshua Wallington <jdwalli2003@yahoo.com>
2026-01-23 20:55:38 -0800

    Update polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,13 +4,6 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar on all monitors
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main --config=~/.config/polybar/config.ini &
-  done
-else
   polybar --reload main --config=~/.config/polybar/config.ini &
-fi
 
 echo "Polybar launched..."
